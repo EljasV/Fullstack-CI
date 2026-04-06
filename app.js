b93476e23ddf251099003931835505e411db83e1
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2026-04-06 18:25:15 +0300

    Prepare for deployment

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express()
 const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
 
 const start = async () => {
   await app.listen(PORT)
